315896bdcd90b3b4bee1f7622dc76c6b8ce37ca3
Nadia Fatah <nadi.fatah@gmail.com>
2022-02-06 22:08:06 +0000

    Implement the has_secure_password based approach to allow visitors to register or log in with a user account

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,17 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
   resources :about, only: [:index]
+  
+  # These routes are for a login form, logging in, and logging out
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+    # receive the form and create a user in our database using the data given to us by the user.
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
