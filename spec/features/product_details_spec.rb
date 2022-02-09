aed7539831c383130102f2435e8b0c747bc28390
Nadia Fatah <nadi.fatah@gmail.com>
2022-02-09 06:26:59 +0000

    Create product detail feature Spec using RSpec, Capybara and a headless Webkit browser powered by PhantomJS

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product detail page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        image: open_asset('apparel1.jpg'),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+    )
+    end
+  end
+
+  scenario "They see all product detail" do
+    # ACT
+    visit root_path
+
+    # CLICK ON
+    first('.btn-default').click
+
+    # commented out b/c it's for debugging only
+    save_and_open_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'article.product-detail'
+  end
+end
